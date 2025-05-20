529c36e52944f11176382dbc10b1bd146b69cf09
Asaki Yuki <vantrong2007vn@gmail.com>
2025-05-20 17:37:22 +0700

    required config

--- asakiyuki.config.cjs
+++ asakiyuki.config.cjs
@@ -3,29 +3,18 @@
  * @type {import('jsonui-scripting').Config}
  */
 const config = {
-    compiler: {
-        autoCompress: false,
-        fileExtension: "json",
-        encodeJson: false,
-        UI: {
-            nameLength: 32,
-            namespaceAmount: 16,
-            namespaceLength: 32,
-            obfuscateName: false,
-            obfuscateType: false,
-        },
-    },
-    installer: {
-        autoInstall: true,
-        developEvironment: true,
-        previewVersion: false,
-        customPath: false,
-        installPath: "/your/minecraft/data/path",
-    },
-    manifest: {
-        name: "Walpaper Generators for BetMC UI",
-        description: "Developed by Asaki Yuki",
-    },
-};
+	compiler: {
+		autoCompress: false,
+		fileExtension: "json",
+	},
+	installer: {
+		autoInstall: true,
+		developEvironment: true,
+	},
+	manifest: {
+		name: "Walpaper Generators for BetMC UI",
+		description: "Developed by Asaki Yuki",
+	},
+}
 
 module.exports = { config }
