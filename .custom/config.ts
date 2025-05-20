67d4f2c81c0aba20a91d32eaefb2f909351786db
Asaki Yuki <vantrong2007vn@gmail.com>
2025-05-20 23:08:34 +0700

    default config

--- .custom/config.ts
+++ .custom/config.ts
@@ -2,7 +2,7 @@ export const CONFIGS: CONFIGS = {
     // input file name (exclude extension)
     input_file_name: "input",
     // fps
-    fps: 30,
+    fps: 24,
     // resolution
     resolution: "1280x720",
     // export type
@@ -14,7 +14,7 @@ export const CONFIGS: CONFIGS = {
 // type
 interface CONFIGS {
     input_file_name: string,
-    fps: FPS,
+    fps: number,
     resolution: RESOLUTION,
     exportType: EXPORT_TYPE,
     quality: QUALITY
@@ -27,4 +27,3 @@ type QUALITY = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 |
     22 | 23 | 24 | 25 | 26 | 27 | 28 | 29 | 30 | 31 | 32 |
     33 | 34 | 35 | 36 | 37 | 38 | 39 | 40 | 41 | 42 | 43 |
     44 | 45 | 46 | 47 | 48 | 49 | 50 | 51;
-type FPS = 15 | 30 | 60;
